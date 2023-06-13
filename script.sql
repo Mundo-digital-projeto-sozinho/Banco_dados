37e29597b4a8afb53a9d9b49226a7441f1ca7784
Winycios <winycios.nascimento@sptech.school>
2023-06-13 01:09:33 -0300

    Mer definitivo e alteração de alguns valores

--- script.sql
+++ script.sql
@@ -8,22 +8,39 @@ CREATE TABLE tbUser(
     email VARCHAR(80),
     senha VARCHAR(12)
 );
-    
+ 
 CREATE TABLE tbDados_pessoais(
 
 	idDados INT PRIMARY KEY AUTO_INCREMENT,
     fk_idUser INT,
     nome VARCHAR(30),
     sobrenome VARCHAR(30),
-    telefone CHAR(14),
-    data_nasc DATE,
+    telefone VARCHAR(18),
+    data_nasc DATE,		  
     FOREIGN KEY (fk_idUser) REFERENCES tbUser(idUser)
     );
-    
+
 CREATE TABLE tbLinguagens (
 
 	idLinguagens INT PRIMARY KEY AUTO_INCREMENT,
     linguagem VARCHAR(45),
+    nivel varchar(30),
     fk_idDados INT,
     FOREIGN KEY (fk_idDados) REFERENCES tbDados_pessoais(idDados)
 );
+
+CREATE TABLE tbComentario(
+
+idComentario INT PRIMARY KEY AUTO_INCREMENT,
+fk_idUser INT, 
+titulo VARCHAR(45),
+comentario VARCHAR(200),
+linguagem VARCHAR(30),
+FOREIGN KEY (fk_idUser) REFERENCES tbUser(idUser)
+);
+
+/* select grafico de barras*/
+select Count(idComentario) as contar, fk_idUser from tbComentario group by fk_idUser;
+
+/* select de pizza*/
+select Count(idComentario) as Contar, linguagem from tbComentario group by linguagem order by Contar Desc;
